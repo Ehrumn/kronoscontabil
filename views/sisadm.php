bc4073b500c646b8fe26a9802cf4902706297e2c
Fernando <Fernando@Fernando-PC>
2017-03-04 20:43:15 -0300

    Merge origin/master
    Conflicts:
    	assets/css/template.css
    	views/login.php

--- views/sisadm.php
+++ views/sisadm.php
@@ -1,15 +1,25 @@
-<div class="conteudo">
-    <div class="bemvindo">Olá <?php //$_SESSION['nome']; ?>, deixe os dados do sistema sempre atualizados.</div>
-    <div class="menu-cadastros">
-
-        <div class="btn-group btn-group-vertical iconInput">
-            <h4>Menu de Cadastros</h4>
-            <a href="clientes" style="text-decoration: none;"><button class="btn" ><i class="glyphicon glyphicon-floppy-disk"></i>Clientes</button></a>
-            <button class="btn">Cadastro</button>
-            <button class="btn">Cadastro</button>
-            <button class="btn">Cadastro</button>
-            <button class="btn">Cadastro</button>
-            <button class="btn">Cadastro</button>
-        </div>   
+<div class="conteudo_all_sisadm">
+    <div class="bemvindo">Olá <?php //$_SESSION['nome'];   ?>, deixe os dados do sistema sempre atualizados.</div>
+    <div class="medlle-um-sisadm">
+        <div class="menu-cadastros">
+            <div class="btn-group btn-group-vertical iconInput">
+                <h4>Menu de Cadastros</h4>
+                <a href="clientes" style="text-decoration: none;"><button class="btn" ><i class="glyphicon glyphicon-floppy-disk"></i>Clientes</button></a>
+                <button class="btn">Cadastro</button>
+                <button class="btn">Cadastro</button>
+                <button class="btn">Cadastro</button>
+                <button class="btn">Cadastro</button>
+                <button class="btn">Cadastro</button>
+            </div>   
+        </div>
+        <div class="conteudo-25-sisadm">Conteudo 2</div>
+        <div class="conteudo-25-sisadm">Conteudo 3</div>
+        <div class="conteudo-25-sisadm">Conteudo 4</div>
+    </div>
+    <div class="medlle-dois-sisadm">
+        <div class="conteudo-25-sisadm">conteudo 1</div>
+        <div class="conteudo-25-sisadm">Conteudo 2</div>
+        <div class="conteudo-25-sisadm">Conteudo 3</div>
+        <div class="conteudo-25-sisadm">Conteudo 4</div>
     </div>
 </div>  
