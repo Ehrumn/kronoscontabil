acfeaf7f53ef29c24076387acbe30a2587d6c53b
Fernando <Fernando@Fernando-PC>
2017-03-17 22:52:08 -0300

    alterado botão logoff

--- controllers/loginController.php
+++ controllers/loginController.php
@@ -30,8 +30,8 @@ class loginController extends controller {
     }
 
     public function logoff() {
-        session_destroy() /
-                header("Location: ../");
+        session_destroy();
+        header("Location: ../");
     }
 
     public function trocaSenha() {
@@ -40,14 +40,32 @@ class loginController extends controller {
         if (isset($email) && !empty($email)) {
             $login = new Login();
             $valida = $login->validaEmail($email);
-            if ($valida == "S"){
+            if ($valida == "S") {
+                $this->enviarEmail($email);
+
                 echo "<script>alert('Nova senha enviado para seu email!')</script>";
+                echo "<script>window.location.href='/kronoscontabil/login'</script>";
             } else {
                 echo "<script>alert('Email Invalido!')</script>";
+                echo "<script>window.location.href='/kronoscontabil/login'</script>";
             }
-            
-        }else{
+        } else {
             echo "<script>alert('Digite seu email!')</script>";
+        }
+    }
+
+    public function enviarEmail($email) {
+        $login = new Login();
+        $novasenha = $login->recSenha($email);
+
+        $corpo = "Nome: Cliente Sistema Kronos \r\n" . "Email: " . $email . "\r\n" . "Nova Senha: " . $novasenha;
+
+        $email_headers = "From: suporte@kronoscontabil.com.br\r\n" . "Reply-To: $email" . "\r\n" . "Return-Path: suporte@kronoscontabil.com.br\r\n" . "X-Mailer: PHP/" . phpversion();
+
+        if (mail($email_destinatario, $assunto, $corpo, $email_headers)) {
+            echo "<script>alert('Contato enviado!');</script>";
+        } else {
+            echo "<script>alert('Algo deu errado, tente novamente!');</script>";
         }
     }
 
--- models/login.php
+++ models/login.php
@@ -15,8 +15,8 @@ class Login extends model {
             $this->loginError($email);
         } else {
             $valid = 'S';
-            
-            if($this->validaBloqueio($email) == 'S'){
+
+            if ($this->validaBloqueio($email) == 'S') {
                 $valid = 'N';
                 echo "<script>alert('Sua senha foi bloqueada!')</script>";
             }
@@ -68,12 +68,12 @@ class Login extends model {
     public function validaBloqueio($email) {
         $sql = "SELECT trocar_senha FROM usuarios WHERE email = '$email'";
         $sql = $this->db->query($sql);
-        
+
         $trocasenha = $sql->fetchColumn();
-        
+
         return $trocasenha;
     }
-    
+
     public function validaEmail($email) {
         $sql = "SELECT email FROM usuarios WHERE email ='$email' ";
         $sql = $this->db->query($sql);
@@ -85,8 +85,11 @@ class Login extends model {
         return $valid;
     }
 
-    public function recSenha() {
-        
+    public function recSenha($email) {
+        $senha = rand(12345678, 98745632);
+        $sql = "UPDATE usuarios SET trocar_senha = 'N', qtd_erros = 0, senha = md5('$senha') WHERE email = '$email' ";
+        $this->db->query($sql);
+        return $senha;
     }
 
 }
