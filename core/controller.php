39e1fbe295d4cb6b3f84267b37a3ebcf243e0501
Fernando <Fernando@Fernando-PC>
2017-03-24 09:48:10 -0300

    alterado botão logoff

--- core/controller.php
+++ core/controller.php
@@ -2,14 +2,7 @@
 
 class controller {
 
-    protected $db;
-
-    public function __construct() {
-        global $config;
-        $this->db = new PDO("mysql:dbname=" . $config['dbname'] . ";host=" . $config['host'], $config['dbuser'], $config['dbpass']);
-    }
-
-    public function loadView($viewName, $viewData = array()) {
+   public function loadView($viewName, $viewData = array()) {
         extract($viewData);
         include 'views/' . $viewName . '.php';
     }
