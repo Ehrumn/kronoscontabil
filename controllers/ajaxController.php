7783c4fe958b4d5820b1daa156322f11990e3cd9
ti-ra <ti-ra@DESKTOP-IGBIMAG>
2017-03-24 09:23:35 -0300

    Merge origin/master
    Conflicts:
    	nbproject/private/private.xml

--- controllers/ajaxController.php
+++ controllers/ajaxController.php
@@ -1,16 +1,21 @@
 <?php
-class ajaxController extends controller{
-    
-    public function index(){
+
+class ajaxController extends controller {
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public function index() {
         $dados = array();
     }
-    
-    public function getUF(){
+
+    public function listauf() {
         $dados = array();
-        
+
         $c = new cidadeEstado();
         $dados['uf'] = $c->getUF();
         print_r($dados['uf']);
         echo json_encode($dados);
     }
+
 }
