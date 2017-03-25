64cddea72f51bdbbb1f3fdaeb03829e193c38953
Fernando <Fernando@Fernando-PC>
2017-03-25 18:03:58 -0300

    Merge origin/master
    Conflicts:
    	views/clientes.php

--- views/cadUsuario.php
+++ views/cadUsuario.php
@@ -3,32 +3,31 @@
     <form action="index.html">
         <!-- area de campos do form -->
         <div class="row">
-            <div class="form-group col-md-6">
-                <label for="campo1">Nome / Razão Social</label>
+            <div class="form-group col-md-4">
+                <label for="campo1">Nome</label>
                 <input type="text" class="form-control" id="nome">
             </div>
 
-            <div class="form-group col-md-6">
-                <label for="campo2">Email</label>
-                <input type="email" class="form-control" id="email">
-            </div>
-
             <div class="form-group col-md-4">
-                <label for="campo3">Endereço</label>
-                <input type="text" class="form-control" id="endereco">
+                <label for="campo2">Email</label>
+                <input type="email" class="form-control" id="email" required>
             </div>
-            <div class="form-group col-md-1">
-                <label for="campo4">Número</label>
+            <div class="form-group col-md-2">
+                <label for="campo3">Senha</label>
+                <input type="text" class="form-control" id="senha">
+            </div>
+            <div class="form-group col-md-2">
+                <label for="campo4">Confirmar Senha</label>
                 <input type="text" class="form-control" id="numero">
             </div>
             <div class="form-group col-md-2">
-                <label for="campo5">Bairro</label>
-                <input type="text" class="form-control" id="bairro">
+                <label for="campo5">Tipo de Usuario</label>
+                <select class="form-control" id="bairro">
+                    <option value=""></option>
+                    <option value="ADMIN">Administrador</option>
+                    <option value="CLIENTE">Cliente</option>
+                </select>
             </div>            
-            <div class="form-group col-md-2">
-                <label for="campo6">UF</label>
-                <input type="text" class="form-control" id="bairro">
-            </div>
         </div>
         <hr />
         <div id="actions" class="row">
