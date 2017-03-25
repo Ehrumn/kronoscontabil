d3e72e4f921557bff4fea89057ccf985fadd5235
Fernando <Fernando@Fernando-PC>
2017-03-25 18:17:06 -0300

    Merge origin/master
    Conflicts:
    	views/clientes.php

--- controllers/cadUsuarioController.php
+++ controllers/cadUsuarioController.php
@@ -7,5 +7,14 @@ class cadUsuarioController extends controller {
         
         $this->loadTemplate('cadUsuario', $dados);
     }
-
+    
+    public function salvarUsuario(){
+        $nome = addslashes($_POST['nome']);
+        $email = addslashes($_POST['email']);
+        $senha = md5(addslashes($_POST['senha'])); 
+        $conf_senha = md5(addslashes($_POST['conf_senha'])); 
+        $tipo = addslashes($_POST['tipo']);
+        echo "<script>alert('enviado');</script>";
+        echo "<script>window.location.href = '/sisadm';</script>";
+    }
 }
--- views/cadUsuario.php
+++ views/cadUsuario.php
@@ -1,28 +1,28 @@
 <div class="container-fluid">
     <h3 class="text-center bg-primary">CADASTRO DE USUARIOS</h3>
-    <form action="index.html">
+    <form method="POST" action="/cadUsuario/salvarUsuario">
         <!-- area de campos do form -->
         <div class="row">
             <div class="form-group col-md-4">
                 <label for="campo1">Nome</label>
-                <input type="text" class="form-control" id="nome">
+                <input type="text" class="form-control" name="nome">
             </div>
 
             <div class="form-group col-md-4">
                 <label for="campo2">Email</label>
-                <input type="email" class="form-control" id="email" required>
+                <input type="email" class="form-control" name="email" required>
             </div>
             <div class="form-group col-md-2">
                 <label for="campo3">Senha</label>
-                <input type="text" class="form-control" id="senha">
+                <input type="text" class="form-control" name="senha">
             </div>
             <div class="form-group col-md-2">
                 <label for="campo4">Confirmar Senha</label>
-                <input type="text" class="form-control" id="numero">
+                <input type="text" class="form-control" name="conf_senha">
             </div>
             <div class="form-group col-md-2">
                 <label for="campo5">Tipo de Usuario</label>
-                <select class="form-control" id="bairro">
+                <select class="form-control" name="tipo">
                     <option value=""></option>
                     <option value="ADMIN">Administrador</option>
                     <option value="CLIENTE">Cliente</option>
